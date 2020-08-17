1916e7c57e212a89a1083f4941df3566f6b9990e
Natsuki Hasegawa <oosacker@gmail.com>
2020-08-17 20:07:20 +1200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  MDBNavbar, 
+  MDBNavbarBrand, 
+  MDBNavbarNav, 
+  MDBNavItem, 
+  MDBNavLink,
+  MDBNavbarToggler, 
+  MDBCollapse, 
+  MDBIcon, 
+} from "mdbreact";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+class NavbarPage extends Component {
+state = {
+  isOpen: false
+};
+
+toggleCollapse = () => {
+  this.setState({ isOpen: !this.state.isOpen });
 }
 
-export default App;
+render() {
+  return (
+    <Router>
+      <MDBNavbar color="indigo" dark expand="md">
+
+        <MDBNavbarBrand>
+          <strong className="white-text">Natsuki</strong>
+        </MDBNavbarBrand>
+
+        <MDBNavbarToggler onClick={this.toggleCollapse} />
+
+        <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+
+          <MDBNavbarNav left>
+
+            <MDBNavItem active>
+              <MDBNavLink to="/">Home</MDBNavLink>
+            </MDBNavItem>
+
+            <MDBNavItem>
+              <MDBNavLink to="/projects">Projects</MDBNavLink>
+            </MDBNavItem>
+
+          </MDBNavbarNav>
+
+          <MDBNavbarNav right>
+            <MDBNavItem>
+              
+              <a href='mailto:natsuki.hasegawa@gmail.com?subject=Contact'>
+                <MDBIcon icon="envelope" className="white-text ml-3"/>
+              </a>
+
+              <a href='https://www.linkedin.com/in/haseganats/'>
+                <MDBIcon fab icon="linkedin" className="white-text ml-3"/>
+              </a>
+              
+            </MDBNavItem>
+          </MDBNavbarNav>
+
+        </MDBCollapse>
+      </MDBNavbar>
+
+      <Switch>
+
+        <Route path="/projects">
+          <>
+            this is the projects page
+          </>
+        </Route>
+
+        <Route path="/">
+          <>
+            this is the home page
+          </>
+        </Route>
+
+      </Switch>
+
+    </Router>
+    );
+  }
+}
+
+export default NavbarPage;
