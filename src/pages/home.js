87b0bd9fe8dd284410d2b7e6f9c7e2538bdfa7bb
Natsuki Hasegawa <oosacker@gmail.com>
2020-08-23 19:05:40 +1200

    update home

--- src/pages/home.js
+++ src/pages/home.js
@@ -14,7 +14,7 @@ const Home = () => {
                 <MDBCol className='profile-container' xl='8' lg='12' top>
                     <div className='profile-container-text z-depth-1-half zoom'>
                         <h3>Profile</h3>
-                        <p>I'm Natsuki Hasegawa, and I've just completed my studies toward a Master of Software Development (MSwDev)degree at the Wellington ICT Graduate School. I also hold a Master of Science (MSc) in Electronics and Computer Systems Engineering, and before making the switch to software, I was an electronics engineer for over 6 years.</p>
+                        <p>I'm Natsuki Hasegawa, a New Zealand-born Japanese male from Wellington. I've just completed my studies toward a Master of Software Development (MSwDev) degree at the Wellington ICT Graduate School. I also hold a Master of Science (MSc) in Electronics and Computer Systems Engineering, and before making the switch to software, I was an electronics engineer for over 6 years. </p>
                     </div>
                 </MDBCol>
 
@@ -23,12 +23,53 @@ const Home = () => {
             <MDBRow>
                 <MDBCol className='skills-container'>
                     <div className='skills-container-text z-depth-1-half zoom'>
-                        <h3>Skills Summary</h3>
-                        <h5>Software Development</h5>
-                        <ul>
-                            <li>Java, Python, JavaSCript, PHP, </li>
-                        </ul>
+
+                        <MDBRow>
+                            <h3>Skills Summary</h3>
+                        </MDBRow>
+
+                        <MDBRow>
+                            <MDBCol>
+                                <h5>Software</h5>
+                                <ul>
+                                    <li>Java</li>
+                                    <li>Python (Flask framework)</li>
+                                    <li>JavaScript (React, Node.js, Express.js)</li>
+                                    <li>PHP (Silverstripe CMS)</li>
+                                    <li>CSS (Sass, Bootstrap)</li>
+                                    <li>Delphi</li>
+                                    <li>C#</li>
+                                    <li>Databases (SQL, MongoDB)</li>
+                                    <li>Microcontroller firmware (ANSI-C, Assembler)</li>
+                                </ul>
+                            </MDBCol>
+                        
+                            <MDBCol>
+                                <h5>Hardware</h5>
+                                <ul>
+                                    <li>PCB design (Altium Designer)</li>
+                                    <li>PCB manufacture (milling machines)</li>
+                                    <li>PCB assembly (manual and reflow soldering)</li>
+                                    <li>Linear power supplies</li>
+                                    <li>Rechargable battery systems (Ni-MH and Ni-Polymer)</li>
+                                    <li>Microcontroller systems</li>
+                                    <li>Metalworking (basic)</li>
+                                </ul>
+                            </MDBCol>
+
+                            <MDBCol>
+                                <h5>Others</h5>
+                                <ul>
+                                    <li>Public speaking</li>
+                                    <li>Full New Zealand Driver Licence</li>
+                                    <li>Translating between Japanese-English languages</li>
+                                </ul>
+                            </MDBCol>
+
+                        </MDBRow>
                     </div>
+
+                    
                 </MDBCol>
             </MDBRow>
 
@@ -42,6 +83,7 @@ const Home = () => {
                                     <th>Period</th>
                                     <th>Company</th>
                                     <th>Position</th>
+                                    <th>Summary</th>
                                 </tr>
                             </MDBTableHead>
                             <MDBTableBody>
@@ -49,16 +91,25 @@ const Home = () => {
                                     <td>March 2020 – July 2020</td>
                                     <td>Signify Limited</td>
                                     <td>Web Developer Intern</td>
+                                    <td>
+                                        Employed full time for 3 months as  final part of my MSwDev degree. Worked on production web development projects, including coding, bug fixing and testing. 
+                                    </td>
                                 </tr>
                                 <tr>
                                     <td>September 2009 – July 2015</td>
-                                    <td>Spark Dental Technology Limited (now Titanium Solutions)</td>
+                                    <td>Spark Dental Technology Limited</td>
                                     <td>Hardware Development Engineer</td>
+                                    <td>
+                                        Worked remotely as the sole full time electronics engineer for a dental software company. Based at the site of Callaghan Innovation (formerly Industrial Research Limited). Worked on ultrasonic imaging systems and portable microcontroller-based electronics. 
+                                    </td>
                                 </tr>
                                 <tr>
                                     <td>March 2009 – August 2009</td>
-                                    <td>Industrial Research Limited (now Callaghan Innovation)</td>
-                                    <td>Research Engineer (fixed-term)</td>
+                                    <td>Industrial Research Limited</td>
+                                    <td>Research Engineer</td>
+                                    <td>
+                                        6-month fixed-term contract after graduating from my MSc. Assisted their ultrasonic imaging programme by designing/assembling printed circuit boards, testing ultrasonic sensors, writing software and doing manual mechanical work.
+                                    </td>
                                 </tr>
                             </MDBTableBody>
                         </MDBTable>
