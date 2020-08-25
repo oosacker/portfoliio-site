f58f5b2fd56ffde7d15ff2730e13272e529560f6
Natsuki Hasegawa <oosacker@gmail.com>
2020-08-25 17:54:42 +1200

    fix the breakpoint

--- src/projectCard.js
+++ src/projectCard.js
@@ -14,7 +14,7 @@ import {
 
 const ProjectCard = (props) => {
     return (
-        <MDBCol className="m-3 z-depth-1-half round-corner" sm="3">
+        <MDBCol className="m-3 z-depth-1-half round-corner" lg="3" md="4" sm="6">
 
             <MDBRow className="px-3 py-2">
                 <img className="img-fluid z-depth-1-half rounded" src={props.image} />
