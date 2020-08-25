7b2a97d04c19a9bfa4c3ca54dfa09642f98cb3fe
Natsuki Hasegawa <oosacker@gmail.com>
2020-08-25 18:02:51 +1200

    center the grid

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -43,7 +43,7 @@ const Projects = () => {
                 </MDBCol>
             </MDBRow>
 
-            <MDBRow>
+            <MDBRow center>
 
                 <ProjectCard 
                     title="TypingTester"
--- src/projectCard.js
+++ src/projectCard.js
@@ -14,9 +14,9 @@ import {
 
 const ProjectCard = (props) => {
     return (
-        <MDBCol className="m-3 z-depth-1-half round-corner" lg="3" md="4" sm="6">
+        <MDBCol className="m-3 z-depth-1-half round-corner" lg="3" md="4" sm="6" xs="12">
 
-            <MDBRow className="px-3 py-2">
+            <MDBRow className="px-3 py-2" center>
                 <img className="img-fluid z-depth-1-half rounded" src={props.image} />
             </MDBRow>
 
