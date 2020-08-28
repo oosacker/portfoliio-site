35c5169720515447030a27eeb7b6cfa2dbfc5ba1
Natsuki Hasegawa <oosacker@gmail.com>
2020-08-28 15:04:22 +1200

    remove around

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -43,7 +43,7 @@ const Projects = () => {
                 </MDBCol>
             </MDBRow>
 
-            <MDBRow left around>
+            <MDBRow left >
 
                 <ProjectCard 
                     title="WeatherChecker"
