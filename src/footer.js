bb6e113cd7f5691b6e6e13a604ba1228c34ee2f9
Natsuki Hasegawa <oosacker@gmail.com>
2020-09-07 11:08:45 +1200

    fixed footer bottom

--- src/footer.js
+++ src/footer.js
@@ -1,47 +1,13 @@
 import React from "react";
-import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+import { MDBContainer, MDBFooter } from "mdbreact";
 
 const Footer = () => {
   return (
       
-    <MDBFooter color="indigo" className="font-small pt-4 mt-4">
-      {/* <MDBContainer fluid className="text-center text-md-left">
-
-        <MDBRow>
-
-          <MDBCol md="6">
-            <h5 className="title">Footer Content</h5>
-            <p>Here you can use rows and columns here to organize your footer content.</p>
-          </MDBCol>
-
-          <MDBCol md="6">
-            <h5 className="title">Links</h5>
-            <ul>
-              <li className="list-unstyled">
-                <a href="#!">Link 1</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Link 2</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Link 3</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Link 4</a>
-              </li>
-            </ul>
-          </MDBCol>
-
-        </MDBRow>
-
-      </MDBContainer> */}
-
-      <div className="footer-copyright py-3">
+    <MDBFooter color="indigo" className="font-small py-2 fixed-bottom">
         <MDBContainer fluid>
-          &copy; {new Date().getFullYear()} Copyright Natsuki Hasegawa
+          &copy; {new Date().getFullYear()} Natsuki Hasegawa
         </MDBContainer>
-      </div>
-
     </MDBFooter>
   );
 }
