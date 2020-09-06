899515abb53bb1f9c86c9948802294fcce413ed3
Natsuki Hasegawa <oosacker@gmail.com>
2020-09-06 22:38:21 +1200

    update hobby text

--- src/pages/hobbies.js
+++ src/pages/hobbies.js
@@ -21,7 +21,13 @@ const Hobbies = () => {
                 <HobbyCard 
                     title="Football"
                     icon={"futbol"}
-                    desc="I am a football fan and currently play indoor soccer at the waterfront. I played for several years in the Wellington Sunday League for Inter Galactic FC, primarily as a right midfielder. Muscle injuries became a problem for me (keep getting leg cramps), so I'm taking a break from 11-vs-11 games."
+                    desc={[
+                        "I am a football fan and currently play ",
+                        <a href="https://www.wis.net.nz/sports/indoor-soccer/indoor-soccer-rules-game-info/">indoor soccer at the waterfront</a>,
+                        ". I played for several years in the ",
+                        <a href="https://www.sportzone.org.nz/wellington-sunday-league/home">Wellington Sunday Football League</a>,
+                        " for Inter Galactic FC, primarily as a right midfielder. Muscle injuries became a problem for me (kept getting leg cramps), so I'm taking a break from 11-vs-11 games. I also regularly visit the local gym for some cardio."
+                    ]}
                     image={"./images/hobby_img/football/1.jpg"}
                 />
             </MDBRow>
@@ -30,7 +36,22 @@ const Hobbies = () => {
                 <HobbyCard 
                     title="Guitar"
                     icon={"guitar"}
-                    desc="I've been playing the guitar since my university days. My dad is also a musician, and I initially learned from him. I own 3 guitars - A Fender Highway One Telecaster (as seen in the photo), a Yamaha FG720S acoustic, and a Yamaha Pacifica electric (broken). I used to copy Bob Dylan's songs when I was young."
+                    desc={[
+                        "I've been playing the guitar since my university days. My dad is also a musician, and I initially learned from him. I own 3 guitars - A ",
+                        <a href="https://www.musicradar.com/reviews/guitars/fender-highway-one-telecaster-25977">Fender Highway One Telecaster</a>,
+                        " (as seen in the photo), a ",
+                        <a href="https://www.musiciansfriend.com/guitars/open-box-yamaha-fg720s-acoustic-guitar">Yamaha FG720S acoustic</a> , 
+                        " and a ", 
+                        <a href= "https://www.musicworks.co.nz/electric-guitars/pac112jbl-yamaha-pac112-pacifica-112-electric-guitar-black/">Yamaha Pacifica</a>,
+                        " electric (broken). I used to copy Bob Dylan's songs when I was young. Some of my favourite bands include ",
+                        <a href= "http://pillows.jp/english/">the pillows</a>,
+                        ", ",
+                        <a href= "https://www.universal-music.co.jp/tmge/"> Thee Michelle Gun Elephant</a>,
+                        " and ",
+                        <a href= "http://spartalocals.net/"> Sparta Locals</a>,
+                        "."
+
+                    ]}
                     image={"./images/hobby_img/guitar/5.jpg"}
                 />
             </MDBRow>
@@ -39,7 +60,12 @@ const Hobbies = () => {
                 <HobbyCard 
                     title="Kendo"
                     icon={"fist-raised"}
-                    desc="I hold a 1st dan grade in kendo, a Japanese martial art involving bamboo swords. I started practicing it in high school. I train at Rembuden Kendo Club, based at Victoria University of Wellington. The photo shows me in a small local competition where I won silver. I'm the one with the red ribbon."
+                    desc={[
+                        "I hold a 1st dan grade in ", 
+                        <a href="https://en.wikipedia.org/wiki/Kendo">kendo</a>, 
+                        ", a Japanese martial art involving bamboo swords. I started practicing it in high school. I train at ", 
+                        <a href="http://www.rembudenkendo.co.nz/">Rembuden Kendo Club</a>, 
+                        " based at Victoria University of Wellington. The photo shows me fighting during a small local competition where I won silver. I'm the one with the red ribbon."]}
                     image={"./images/hobby_img/kendo/3.jpg"}
                 />
             </MDBRow>
