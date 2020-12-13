3a6fe041f6c7e446c4684a23e5909a0b0ecee811
Natsuki Hasegawa <nat@sunnysideup.co.nz>
2020-12-13 20:49:19 +1300

    updated about and script

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,7 @@ const About = () => {
     return (
         <MDBContainer>
               <p>This website was created from scratch (no templates used!) with React (create-react-app). It is hosted on <a href="http://heroku.com">Heroku</a> and is copyrighted to Natsuki Hasegawa, 2020.</p>
-              <p>Some info:</p>
+              <p>Some info:</p> 
               <ul>
                   <li><a href="https://mdbootstrap.com/">Material Design for Bootstrap UI library</a> (React version) was used for the user interface.</li>
                   <li>I always use modern functional components with hooks in React, instead of classes - this web app is no exception. This makes the code simpler and cleaner.</li>
@@ -14,7 +14,7 @@ const About = () => {
                   <li><a href="https://reactrouter.com/">React-Router</a> is used for the navbar - notice how the URL changes when you click on the different links, despite being a single page web app.</li>
                   <li><a href="https://github.com/mars/create-react-app-buildpack">Heroku Buildpack for create-react-app</a> was used to deploy this web app to Heroku.</li>
                   <li>The source code for this website can be seen on <a href="https://github.com/oosacker/portfolio-site">GitHub</a>.</li>
-                  <li>The hosting is provided by <a href="http://heroku.com">Heroku</a> while the domain name is managed by <a href="http://hover.com">Hover</a>. Connecting 2 separate services wasn't so simple, especially with Heroku's dyno system that doesn't provide a static IP address (which can be used for an A-Record). To do this, DNS targets (CNAME record) had to be generated on Heroku and configured on Hover's DNS settings. You may not know this, but the "root" address (http://natsuki.co.nz) and "www" address (http://www.natsuki.co.nz) is not the same thing!</li>
+                  <li>The hosting is provided by <a href="http://heroku.com">Heroku</a> while the domain name is managed by <a href="http://hover.com">Hover</a>. Connecting 2 separate services wasn't so simple, especially with Heroku's dyno system that doesn't provide a static IP address (which can be used for an A-Record). To do this, DNS targets (CNAME record) had to be generated on Heroku and configured on Hover's DNS settings. I also use <a href="https://cloudflare.comk">Cloudflare</a> to manage the DNS.</li>
               </ul>
         </MDBContainer>
     );
